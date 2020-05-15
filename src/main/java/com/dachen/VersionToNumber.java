b4c67e1f15d0dac02feaaa015187e0c92f2f70d6
lijun <lijun@dachentech.com.cn>
2020-05-15 16:08:13 +0800

    app版本大小比较

---
+++ src/main/java/com/dachen/VersionToNumber.java
@@ -0,0 +1,45 @@
+package com.dachen;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class VersionToNumber extends UDF {
+
+    public int evaluate(String v1, String v2) {
+        if (v1.equals(v2)) {
+            return 0;
+        }
+        String[] version1Array = v1.split("\\.");
+        String[] version2Array = v2.split("\\.");
+        int index = 0;
+        int minLen = Math.min(version1Array.length, version2Array.length);
+        long diff = 0;
+
+        while (index < minLen
+                && (diff = Long.parseLong(version1Array[index])
+                - Long.parseLong(version2Array[index])) == 0) {
+            index++;
+        }
+        if (diff == 0) {
+            for (int i = index; i < version1Array.length; i++) {
+                if (Long.parseLong(version1Array[i]) > 0) {
+                    return 1;
+                }
+            }
+
+            for (int i = index; i < version2Array.length; i++) {
+                if (Long.parseLong(version2Array[i]) > 0) {
+                    return -1;
+                }
+            }
+            return 0;
+        } else {
+            return diff > 0 ? 1 : -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int a = new VersionToNumber().evaluate("11", "10");
+        System.out.println(a);
+
+    }
+}
